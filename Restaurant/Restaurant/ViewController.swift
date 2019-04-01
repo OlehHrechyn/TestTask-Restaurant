95824fe42129cdc8faf78d2250b7a47dadd79a5a
Oleh Hrechyn <oleh_hrechyn@natife.com>
2019-04-01 17:52:18 +0300

    Initial commit

---
+++ Restaurant/Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Oleh Hrechyn on 4/1/19.
+//  Copyright © 2019 Natife. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
